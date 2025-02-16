d36c7e77663b251960f29b746c44110953583af9
Abhijeet Ingle <ingleabhi.ui@gmail.com>
2025-02-16 17:02:34 +0530

    updated index file again

--- backend/index.js
+++ backend/index.js
@@ -33,6 +33,6 @@ async function main() {
 
 main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(process.env.PORT||port, () => {
+  console.log(`Example app listening on port `);
 });
